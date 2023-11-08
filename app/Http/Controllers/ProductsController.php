5b1fc3fdce41210c97c9b376ab374ddbeda9c3c0
fauzanreza <fauzan.rez@gmail.com>
2023-11-08 21:34:36 +0700

    add update and delete

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -38,13 +38,29 @@ class ProductsController extends Controller
     return new ProductResource($product);
     }
 
-    public function update(Request $request, string $id)
+    public function update(Request $request, Products $product)
     {
-        //
+        $product_name = $request->input('name');
+        $product_price = $request->input('price');
+        $product_description = $request->input('description');
+        $product_stock = $request->input('stock');
+        $product_image_url = $request->input('image_url');
+
+        $product->update([
+            'name' => $product_name,
+            'price' => $product_price,
+            'description' => $product_description,
+            'stock' => $product_stock,
+            'image_url' => $product_image_url,
+        ]);
+            return response()->json([
+                'data' => new ProductResource($product)
+            ], 200);
     }
-    
-    public function destroy(string $id)
+
+    public function destroy(Products $product)
     {
-        //
-    }
+        $product->delete();
+        return response()->json(null,204);
+     }
 }
--- routes/api.php
+++ routes/api.php
@@ -22,5 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/products', [ProductsController::class, 'index'])->name('products.index');
 Route::get('/products/{product}', [ProductsController::class, 'show'])->name('products.show');
 Route::post('/products', [ProductsController::class, 'store'])->name('products.store');
-// Route::put('products/{product}', [ProductsController::class, 'update'])->name('products.update');
-// Route::delete('products/{product}', [ProductsController::class, 'destroy'])->name('products.destroy');
+Route::put('products/{product}', [ProductsController::class, 'update'])->name('products.update');
+Route::delete('products/{product}', [ProductsController::class, 'destroy'])->name('products.destroy');
